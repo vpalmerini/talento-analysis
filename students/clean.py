5878097678bfe23d9d1a1b9d1d83cf6f28d51583
vpalmerini <victorpalmerini@gmail.com>
2018-11-06 12:40:58 -0200

    add string matching for cities and referrers

--- students/clean.py
+++ students/clean.py
@@ -5,7 +5,30 @@ from fuzzywuzzy import fuzz, process
 df = pd.read_csv('MailingTalento2018.csv', usecols=['profile.full_name', 'profile.email', 'application.phone', 'application.school', 'application.course', 'application.enroll_year', 'application.cv', 'application.cv2'])
 
 # renaming columns
-df = df.rename(index=str, columns={'profile.full_name':'NomeCompleto', 'profile.email':'Email', 'application.phone':'Telefone', 'application.school':'Faculdade', 'application.course':'Curso', 'application.enroll_year':'AnoDeEntrada', 'application.cv':'Currículo1', 'application.cv2':'Currículo2'})
+df = df.rename(index=str, columns={
+								   'application.can_move':'Pode se mudar',
+								   'application.education':'Escolaridade',
+								   'application.english_level':'Inglês',
+								   'application.excel_level':'Excel',
+								   'application.extra_courses':'Cursos Extras',
+								   'application.first_timer':'Primeira Vez',
+								   'application.gender':'Gênero',
+								   'application.interests':'Interesses',
+								   'application.other_languages':'Outros Idiomas',
+								   'application.referrer':'Referência',
+								   'application.state':'Estado',
+								   'profile.state':'Status',
+								   'profile.full_name':'NomeCompleto',
+								   'application.age':'Idade',
+								   'profile.email':'Email', 
+								   'application.phone':'Telefone',
+								   'application.city':'Cidade',
+								   'application.country':'País',
+								   'application.school':'Faculdade', 
+								   'application.course':'Curso', 
+								   'application.enroll_year':'AnoDeEntrada', 
+								   'application.cv':'Currículo1', 
+								   'application.cv2':'Currículo2'})
 
 # drop null values
 df = df.dropna(subset=['NomeCompleto', 'Email', 'Curso'])
@@ -14,8 +37,33 @@ df = df.dropna(subset=['NomeCompleto', 'Email', 'Curso'])
 df = df.drop_duplicates(subset=['Email'], keep='first')
 
 # reordering columns
-df = df[['NomeCompleto', 'Email', 'Telefone','Currículo1', 'Currículo2', 'AnoDeEntrada', 'Curso', 'Faculdade']]
+df = df[['NomeCompleto', 
+		 'Email', 
+		 'Telefone', 
+		 'Idade', 
+		 'Gênero', 
+		 'Escolaridade',
+		 'Currículo1', 
+		 'Currículo2', 
+		 'AnoDeEntrada', 
+		 'Curso', 
+		 'Faculdade',
+		 'Cidade',
+		 'Estado',
+		 'País',
+		 'Inglês',
+		 'Excel',
+		 'Cursos Extras',
+		 'Outros Idiomas',
+		 'Interesses',
+		 'Pode se mudar',
+		 'Primeira Vez',
+		 'Referência',
+		 'Status'
+		 ]]
 
+
+# string matching - courses
 courses = 	['Administração', 
 			'Análise de Sistemas', 
 			'Arquitetura',
@@ -78,7 +126,6 @@ courses = 	['Administração',
 			'Sistemas de Informação'
 			]
 
-# string matching - courses
 for i in range(len(df['Curso'])):
 	for course in courses:
 		if (fuzz.token_set_ratio(course, df['Curso'][i]) > 90):
@@ -86,6 +133,7 @@ for i in range(len(df['Curso'])):
 
 
 
+# string matching - universities
 universities =	['Unicamp',
 				'USP',
 				'Unesp',
@@ -100,11 +148,41 @@ universities =	['Unicamp',
 				'UFSCAR',
 				]
 
-# string matching - universities
 for i in range(len(df['Faculdade'])):
 	for university in universities:
 		if (fuzz.token_set_ratio(university, df['Faculdade'][i]) > 90):
 			df['Faculdade'][i] = university
+
+
+# string matching - cities
+cities = ['Campinas',
+		  'São Paulo',
+		  'Limeira',
+		  'São Carlos',
+		  'Bauru',
+		  'São José dos Campos',
+		  'Piracicaba',
+		  'Jundiaí',
+		]
+
+for i in range(len(df['Cidade'])):
+	for city in cities:
+		if (fuzz.token_set_ratio(city, df['Cidade'][i]) > 90):
+			df['Cidade'][i] = city
+
+
+# string matching - referrers
+referrers = ['Facebook',
+			 'Email',
+			 'Amigos'
+			 'Unicamp',
+			 'Universidade'
+		]
+
+for i in range(len(df['Referência'])):
+	for referrer in referrers:
+		if (fuzz.token_set_ratio(referrer, df['Referência'][i]) > 90):
+			df['Referência'][i] = referrer
 
 
 # export to csv
